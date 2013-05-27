c6833dc2536cd7eaa04dc549aff06e895dcd7795
Alessandro Luca Asoni <ale.luca.asoni@gmail.com>
2013-05-27 12:17:11 -0400

    new branch, started word kernel

--- pairedkmersvm_kmatrix.py
+++ pairedkmersvm_kmatrix.py
@@ -24,145 +24,123 @@ from itertools import product
 from libkmersvm import *
 
 
-def get_kmer_list(k):
-    """ Genrate list of all possible kmers of length k 
+class Index(object):
+    """ Substring index using hash table map """
+    def __init__(self, t, ln=6):
+        """ Create index, extracting substrings of length 'ln' """
+        self.ln = ln
+        self.index = {}
+        for i in xrange(0, len(t)-ln+1):
+            substr = t[i:i+ln]
+            if substr not in self.index:
+                self.index[substr] = []
+            self.index[substr].append(i)
+
+    def query(self, p):
+        """ Return candidate alignments for p """
+        return self.index.get(p[:self.ln]) or []
+
+def kmerid2kmer(kmerid, kmerlen):
+	"""convert integer kmerid to kmer string
+
+	Arguments:
+	kmerid -- integer, id of k-mer
+	kmerlen -- integer, length of k-mer
+
+	Return:
+	kmer string
+	"""
+
+	nts = "ACGT"
+	kmernts = []
+	kmerid2 = kmerid
+
+	for i in xrange(kmerlen):
+		ntid = kmerid2 % 4
+		kmernts.append(nts[ntid])
+		kmerid2 = int((kmerid2-ntid)/4)
+
+	return ''.join(reversed(kmernts))
+
+
+def kmer2kmerid(kmer, kmerlen):
+	"""convert kmer string to integer kmerid
+
+	Arguments:
+	kmerid -- integer, id of k-mer
+	kmerlen -- integer, length of k-mer
+
+	Return:
+	id of k-mer
+	"""
+
+	nt2id = {'A':0, 'C':1, 'G':2, 'T':3}
+
+	return reduce(lambda x, y: (4*x+y), [nt2id[x] for x in kmer])
+
+
+def get_rcmap(kmerid, kmerlen):
+	"""mapping kmerid to its reverse complement k-mer on-the-fly
+
+	Arguments:
+	kmerid -- integer, id of k-mer
+	kmerlen -- integer, length of k-mer
+
+	Return:
+	integer kmerid after mapping to its reverse complement
+	"""
+
+	#1. get kmer from kmerid
+	#2. get reverse complement kmer
+	#3. get kmerid from revcomp kmer
+	rckmerid = kmer2kmerid(revcomp(kmerid2kmer(kmerid, kmerlen)), kmerlen)
+
+	if rckmerid < kmerid:
+		return rckmerid
+
+	return kmerid
+
+
+def word_kernel( seq1, ind2, k, dmin, dmax ):
+    """ Calculate kernel function applied to the inputs 
     Arguments:
-        k -- int, kmer length
+        seq1 -- string, first sequence
+        ind2 -- index, index for second sequences
 
     Return:
-        list of all possible kmers of length k
+        kernel function of seq1 and seq2
     """
-    kmerlist = []
-    for x in product('ACGT', repeat=k):
-        kmerlist.append(''.join(x))
-    return kmerlist
+    dot_product = 0
+    for i in xrange(0, len(seq1)-k+1):
+        a = seq1[i:i+k]
+        a_in_2 = ind2.query(a) + ind2.query(revcomp(a))
+        if not a_in_2: continue
 
-
-def kpair2feat_map(kmers, homeopair, quiet):
-    """ Create kmair-pair to feature id map 
-    Arguments:
-        kmers -- list of strings, list of all single kmers
-        homeopair -- boolean, if true a pair of equal kmers is used as feature
-        quiet -- boolean, if true it suppresses messages
-
-    Return:
-        dictionary of kmer-pairs (a,b) to feature id number
-    """
-    if not quiet:
-        print "creating kmer-pair to feature id map ... "
-    feature_id = 1
-    kpair_id_dict = {}
-    #variables to keep track of progress
-    progress_count = 0
-    total = len(kmers)
-    for a in kmers:
-        if not quiet and progress_count % math.ceil(total/100.0) == 0:
-            p = (float(progress_count)/total)*100
-            sys.stdout.write("\r%.2f%% progress. " % p )
-            sys.stdout.flush()
-        progress_count += 1
-
-        for b in kmers:
-            if not homeopair:
-                if a == revcomp(b): continue
-                if a == b:          continue
-
-            if (a,b) in kpair_id_dict: continue
-            if (b,a) in kpair_id_dict: continue
-
-            kpair_id_dict[ (a,b) ] = feature_id
-            kpair_id_dict[ (a,revcomp(b)) ] = feature_id
-            kpair_id_dict[ (revcomp(a),b) ] = feature_id
-            kpair_id_dict[ (revcomp(a),revcomp(b)) ] = feature_id
-
-            feature_id += 1
-    if not quiet:
-        print feature_id
-    return kpair_id_dict
-
-
-def create_feature_vector( seq, features, k, dmin, dmax):
-    """ Create SVM-light format feature vector 
-    Arguments:
-        seq -- string, DNA sequence
-        features -- dictionary, kmer pair to feature id map
-        k -- integer, kmer length
-        dmin -- integer, mininmum distance b/w kmer pair
-        dmax -- integer, maximum distance b/w kmer pair
-
-    Return:
-        feature vector for seq
-    """
-    feature_vector = {}
-    for i in xrange(0, len(seq)-k+1):
-        a = seq[i:i+k]
-        left  = seq[max(0, i-(dmax+k)):max(0, i-dmin)]
-        right = seq[min(len(seq),i+k+dmin):min(len(seq),i+dmax+k+k)]
+        left  = seq1[max(0, i-(dmax+k)):max(0, i-dmin)]
+        right = seq1[min(len(seq1),i+k+dmin):min(len(seq1),i+dmax+k+k)]
         #checking for pairs to the left of 'a'
         for j in xrange(0, len(left)-k+1):
             b = left[j:j+k]
-            if (a,b) in features:
-                feature_id = features[(a,b)]
-            elif (b,a) in features:
-                feature_id = features[(b,a)]
-            else: continue
+            b_in_2 = ind2.query(b) + ind2.query(revcomp(b))
+            if not b_in_2: continue
 
-            if feature_id not in feature_vector:
-                feature_vector[feature_id] = 0
-            feature_vector[feature_id] += 1
+            for m in a_in_2:
+                for n in b_in_2:
+                    if abs(m - n) <= dmax:
+                        dot_product += 1
 
         #checking for pairs to the right of 'a'
         for j in xrange(0, len(right)-k+1):
             b = right[j:j+k]
-            if (a,b) in features:
-                feature_id = features[(a,b)]
-            elif (b,a) in features:
-                feature_id = features[(b,a)]
-            else: continue
+            b_in_2 = ind2.query(b) + ind2.query(revcomp(b))
+            if not b_in_2: continue
 
-            if feature_id not in feature_vector:
-                feature_vector[feature_id] = 0
-            feature_vector[feature_id] += 1
+            for m in a_in_2:
+                for n in b_in_2:
+                    if abs(m - n) <= dmax:
+                        dot_product += 1
 
-    return feature_vector
-
-
-def spectrum_kernel( x1, x2, mag_x1, mag_x2 ):
-    """ Calculate the spectrum kernel function applied to the inputs 
-    Arguments:
-        x1 -- map, feature vector
-        x2 -- map, feature vector
-        mag_x1 -- float, magnitude of x1
-        mag_x2 -- float, magnitude of x2
-
-    Return:
-        <x1,x2>/(|x1||x2|)
-    """
-    return inner_product(x1,x2)/(mag_x1*mag_x2)
-
-
-def inner_product( x1, x2 ):
-    """ Calculate the inner product of theh inputs 
-    Arguments:
-        x1 -- map, feature vector
-        x2 -- map, feature vector
-
-    Return:
-        <x1,x2>
-    """
-    if len(x1) < len(x2):
-        X = x1
-        Y = x2
-    else:
-        X = x2
-        Y = x1
-
-    in_product = 0
-    for ( f_id, f_count) in X.items():
-        if f_id in Y:
-            in_product += f_count * Y[f_id]
-
-    return in_product
+    return dot_product
 
 
 def create_kernel_matrix(n):
@@ -177,7 +155,7 @@ def create_kernel_matrix(n):
     return M
 
 
-def fill_kernel_matrix(seqs,M,features,k,dmin,dmax,quiet):
+def fill_kernel_matrix(seqs,M,k,dmin,dmax,quiet):
     """ Fill kernel matrix 
     Arguments:
         seqs -- list of strings, DNA sequences 
@@ -193,40 +171,37 @@ def fill_kernel_matrix(seqs,M,features,k,dmin,dmax,quiet):
     if not quiet:
         print "filling kernel matrix ... "
     
-    #memorize feature vectors
-    feature_vectors = [{}] * len(seqs)
-    #memorize vector magnitudes
-    vector_mags = [0.0] * len(seqs)
+    #memorize index for each sequence
+    indices = [None] * len(seqs)
+    #memorize squared magnitudes
+    square_mags = [0.0] * len(seqs)
 
     #variables to keep trak of progress
     progress_count = 0
-    total = len(seqs)
+    total = ( (len(seqs)+2)*(len(seqs)+1) )/2.0
     for i in xrange(0,len(seqs)):
-        if not quiet and progress_count % math.ceil(total/1000.0) == 0:
-            p = (float(progress_count)/total)*100
-            sys.stdout.write("\r%.2f%% progress. " % p)
-            sys.stdout.flush()
-        progress_count += 1
-        for j in xrange(0,i+1):
-            if not len(feature_vectors[i]) == 0:
-                xi = feature_vectors[i]
-                mag_xi = vector_mags[i]
-            else:
-                xi = create_feature_vector(seqs[i],features,k,dmin,dmax)
-                mag_xi = math.sqrt(float(inner_product(xi,xi)))
-                feature_vectors[i] = xi
-                vector_mags[i] = mag_xi 
+        for j in xrange(i,-1,-1):
+            if not quiet and progress_count % math.ceil(total/10000.0) == 0:
+                p = (float(progress_count)/total)*100
+                sys.stdout.write("\r%.2f%% progress. " % p)
+                sys.stdout.flush()
+            progress_count += 1
+            
+            if 
 
-            if not len(feature_vectors[j]) == 0:
-                xj = feature_vectors[j]
-                mag_xj = vector_mags[j]
+            if indices[j]:
+                indj = indices[j]
+                sqmj = square_mags[j]
             else:
-                xj = create_feature_vector(seqs[j],features,k,dmin,dmax)
-                mag_xj = math.sqrt(float(inner_product(xj,xj)))
-                feature_vectors[j] = xj
-                vector_mags[j] = mag_xj 
+                indj = Index(seqs[j],k)
+                indeces[j] = indj
 
-            M[i,j] = spectrum_kernel( xi, xj, mag_xi, mag_xj )
+            krnl = word_kernel( seqs[i], indj, k, dmin, dmax)
+            if i == j:
+                M[i,j] = 1.0
+            else 
+
+            
             
     if not quiet:
         print
@@ -291,10 +266,8 @@ def main(argv = sys.argv):
     seqs = seqs_pos + seqs_neg
     sids = sids_pos + sids_neg
 
-    kpair_map = kpair2feat_map( get_kmer_list(options.kmerlen), options.homeopair, options.quiet )
-
     M = create_kernel_matrix(len(seqs))
-    M = fill_kernel_matrix(seqs,M,kpair_map,options.kmerlen, options.dmin, options.dmax, options.quiet)
+    M = fill_kernel_matrix(seqs,M,options.kmerlen, options.dmin, options.dmax, options.quiet)
     write_kernel_matrix(M,len(seqs),outm)
 
 if __name__ =='__main__': main()
